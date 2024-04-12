2a3153a96811165e62577ab3861cdd85b8141fcd
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-04-12 17:03:28 +0200

    writter small corrections

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -4,12 +4,12 @@ import java.io.FileWriter;
 import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
-    public void writeSymptoms() {
+    public void writeSymptoms(Map<String, Integer> occurrence) {
         try {
             FileWriter writer = new FileWriter ("result.out");
             Map<String, Integer> occurrence = new TreeMap<>();
             for (Map.Entry<String, Integer> entry : occurrence.entrySet()) {
-                writer.write(entry.getValue() + ": " + entry.getKey().toString());
+                writer.write(entry.getKey() + ": " + entry.getValue().toString());
             }
                 
             writer.close();
