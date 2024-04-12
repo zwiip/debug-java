eefdc9421de1d75da61bc3d4d9c94b97e49b4889
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-04-12 16:50:30 +0200

    writer implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,5 +1,20 @@
 package com.hemebiotech.analytics;
 
+import java.io.FileWriter;
+import java.util.Map;
+
 public class WriteSymptomDataToFile implements ISymptomWriter {
-    
+    public void writeSymptoms() {
+        try {
+            FileWriter writer = new FileWriter ("result.out");
+            Map<String, Integer> occurrence = new TreeMap<>();
+            for (Map.Entry<String, Integer> entry : occurrence.entrySet()) {
+                writer.write(entry.getValue() + ": " + entry.getKey().toString());
+            }
+                
+            writer.close();
+        } catch {
+
+        }
+    }
 }
