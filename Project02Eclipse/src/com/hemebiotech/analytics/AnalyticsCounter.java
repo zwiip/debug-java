3a4cc1454f6bbcd3035a4d40be767e14ca6f5b8d
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-04-18 15:09:16 +0200

    commenting the code in order to refactor it

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
+	/* private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 	
@@ -32,7 +32,7 @@ public class AnalyticsCounter {
 
 			line = reader.readLine();
 		}
-		/*
+		
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
