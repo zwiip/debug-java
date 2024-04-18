73ddf67eb63d86157bd8281aeb87a0ab7fc75fd8
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-04-18 14:56:19 +0200

    corrections and error management

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -2,19 +2,21 @@ package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
 import java.util.Map;
+import.java.util.TreeMap;
+import java.io.IOException;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
     public void writeSymptoms(Map<String, Integer> occurrence) {
         try {
             FileWriter writer = new FileWriter ("result.out");
-            Map<String, Integer> occurrence = new TreeMap<>();
-            for (Map.Entry<String, Integer> entry : occurrence.entrySet()) {
-                writer.write(entry.getKey() + ": " + entry.getValue().toString());
+            TreeMap<String, Integer> sortedMap = new TreeMap<>(occurrence);
+            for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue());
             }
                 
             writer.close();
-        } catch {
-
+        } catch (IOException e) {
+            System.err.println("Error writing the file: " + e.getMessage());
         }
     }
 }
