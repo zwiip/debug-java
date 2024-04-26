93c9b5f201a64f612389cd34c2dfc5eae0c50ca4
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-04-26 12:08:11 +0200

    cleaning comments and imports

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,14 +1,8 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.List;
-import com.hemebiotech.analytics.ISymptomReader;
-import com.hemebiotech.analytics.ISymptomWriter;
-
 
 public class AnalyticsCounter {
 	private ISymptomReader reader;
@@ -38,39 +32,4 @@ public class AnalyticsCounter {
 		writer.writeSymptoms(symptoms);
 	}
 
-	/* private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();
-		}
-		
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
-	*/
 }
