0d8e04fccbd505208d08156a50a4e4c54038c012
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-04-26 15:48:40 +0200

    main implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,8 +1,18 @@
 package com.hemebiotech.analytics;
 
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
+        AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+
+        List<String> symptomsInput = counter.getSymptoms();
+        Map<String, Integer> symptomsResult = counter.countSymptoms(symptomsInput);
+        counter.writeSymptoms(symptomsResult);
     }
     
 }
