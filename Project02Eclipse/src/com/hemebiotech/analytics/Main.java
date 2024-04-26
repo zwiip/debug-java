e9aa68e2af5a468a23b00aff87897c528bf08ce9
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-04-26 22:54:11 +0200

    Adding filepath logic and exceptions' management

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,17 +1,38 @@
 package com.hemebiotech.analytics;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
-        ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
-        AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+        String filePath;
+        if (args.length == 0) {
+            filePath = "../symptoms.txt";
+        } else {
+            filePath = args[0];
+        }
+        
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException("File not found. Please restart the application with a correct file path.");
+            }
 
-        List<String> symptomsInput = counter.getSymptoms();
-        Map<String, Integer> symptomsResult = counter.countSymptoms(symptomsInput);
-        counter.writeSymptoms(symptomsResult);
+            ISymptomReader reader = new ReadSymptomDataFromFile(filePath);
+            ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
+            AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+
+            List<String> symptomsInput = counter.getSymptoms();
+            Map<String, Integer> symptomsResult = counter.countSymptoms(symptomsInput);
+            counter.writeSymptoms(symptomsResult);
+            System.out.println("result.out has been correctly written.");
+
+        } catch (FileNotFoundException e) {
+                System.err.println(e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
+        }
     }
 }
